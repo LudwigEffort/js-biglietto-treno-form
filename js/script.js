18a762479a5cfa3445caf976e58a688a4e877d49
LudwigVonEffort <dr.feynman65@gmail.com>
2022-10-21 17:02:35 +0200

    button don't work

--- js/script.js
+++ js/script.js
@@ -27,13 +27,13 @@ else console.log(`il prezzo è: ${priceTot.toFixed(2)} €`);
 */
 
 
-let numberKM = parseInt(document.querySelector('[name=distance]').value);
+let numberKM = parseInt(document.getElementById('distanceKM').value);
 
 const makeTiket = document.getElementById('btnTiket');
 
 makeTiket.addEventListener('click',
 
-    function() {
+    function () {
         document.getElementById('tiketPrice').innerHTML = `${numberKM}`;
     }
 
